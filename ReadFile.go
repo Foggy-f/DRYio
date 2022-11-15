6a0f0560aa8524f6612ba18be5474583addc4d22
foggy-f <114947576+Foggy-f@users.noreply.github.com>
2022-11-15 16:51:06 +0300

    изменено:      ReadFile.go

--- ReadFile.go
+++ ReadFile.go
@@ -7,6 +7,28 @@ import (
 	"strings"
 )
 
+func ReadF(S string) []byte {
+
+	file, err := os.Open(S)
+	//очищаем S от данных, так как она используется далее
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	data := make([]byte, 5000)
+	B := data
+	for {
+		n, err := file.Read(data)
+		if err == io.EOF {
+			break
+		}
+		//склеиваем в одну строку
+		B = append(B, data[:n]...)
+	}
+	return B
+}
 func ReadString(S string) string {
 
 	file, err := os.Open(S)
