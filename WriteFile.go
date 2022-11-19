16706e65d833521cad63819e3a58bacd3152f0af
foggy-f <114947576+Foggy-f@users.noreply.github.com>
2022-11-19 11:18:27 +0300

    изменено:      WriteFile.go

--- WriteFile.go
+++ WriteFile.go
@@ -22,6 +22,22 @@ func WriteF(S string, data []byte) int {
 
 	return N
 }
+func ReWriteF(S string, data []byte) int {
+
+	file, err := os.Create(S)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	N, err := file.Write(data)
+	if err != nil {
+		fmt.Println(file, err)
+	}
+
+	return N
+}
 
 func WriteNew(S string) string {
 
