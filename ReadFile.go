ad73f3de5510a4ba728cc7147aac49b09d556299
foggy-f <114947576+Foggy-f@users.noreply.github.com>
2022-12-26 12:08:30 +0300

    изменено:      ReadFile.go

--- ReadFile.go
+++ ReadFile.go
@@ -1,12 +1,14 @@
 package DRYio
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
 	"strings"
 )
-//read byte slise
+
+// read byte slise
 func ReadByteSlise(S string) []byte {
 
 	file, err := os.Open(S)
@@ -56,4 +58,40 @@ func ReadStrSlise(S string) []string {
 	words := strings.Fields(ReadString(S)) //преобразуем строку ,разделенную пробелами, в слайс
 
 	return words
+}
+func ReadStringFromSTD(s string) (string, error) {
+
+	file, err := os.Open(s)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	r := bufio.NewScanner(file)
+	// handle first encountered error while reading
+	if err = r.Err(); err != nil {
+		err = fmt.Errorf("error while reading file %s: %s", s, err)
+		return "", err
+	}
+	st := ""
+	for r.Scan() {
+		st = r.Text()
+	}
+
+	return st, err
+}
+
+func WaitInput() {
+
+	r := bufio.NewScanner(os.Stdin)
+	// handle first encountered error while reading
+	if err := r.Err(); err != nil {
+		return
+	}
+	fmt.Println("Нажмите любую клаишую")
+	for r.Scan() {
+		if r.Text() != "" {
+			break
+		}
+	}
 }
